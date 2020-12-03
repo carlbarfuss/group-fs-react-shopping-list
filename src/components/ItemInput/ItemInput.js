40b86a892beaa4ad35a684634d23dc29571bc237
John Micko <jrmicko5012@gmail.com>
2020-12-03 16:36:41 -0600

    Add form skeleton

--- src/components/ItemInput/ItemInput.js
+++ src/components/ItemInput/ItemInput.js
@@ -4,7 +4,16 @@ class ItemInput extends Component{
     render(){
         return(
             <div>
-                <form></form>
+                <h2>Add an Item</h2>
+                <form>
+                    <label>Item: </label>
+                    <input type="text" />
+                    <label>Quantity: </label>
+                    <input type="number" />
+                    <label>Unit: </label>
+                    <input type="text" />
+                    <button type="submit">Save</button>
+                </form>
             </div>
         ) // end return
     } // end render
