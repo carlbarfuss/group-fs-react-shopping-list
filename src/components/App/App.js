e4907437206c03df10475967a767bab551bd5123
julianbooher <julianbooher@hotmail.com>
2020-12-03 13:44:52 -0600

    get route up, axios installed, shopping list component created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
+import ShoppingList from '../ShoppingList/ShoppingList';
 
 class App extends Component {
+
+  state = {
+    shoppingList: []
+  }
+
+  getShoppingList = () => {
+    axios.get('/list')
+    .then ( (response) => {
+      console.log('Response:', response.data);
+      this.setState({
+        shoppingList: response.data
+      })
+    })
+    .catch( (error) => {
+      alert('Something bad happened');
+      console.log('Error', error)
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
